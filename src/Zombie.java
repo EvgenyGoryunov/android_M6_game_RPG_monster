d092ba10063a21aa0b6a3e2bc9769a2665c4838f
Evgeny_Goryunov <ges987@list.ru>
2021-11-25 13:33:15 +0700

    1. start new branch monster

--- src/Zombie.java
+++ src/Zombie.java
@@ -1,13 +1,15 @@
 public class Zombie extends Monster {
 
 
+    public static String scream = "Raaaauuughhhh";
+
     public Zombie(String name) {            //конструктор класс, созданный на основе конструктора суперкласса
         super(name + " the Zombie", 5);
     }
 
 
     public void growl(){
-        System.out.print("Raaaauuughhhh");
+        System.out.print(scream);
         super.growl();
     }
 
